0723dae318d09ca37dbd78c526c28e8dafae7c37
Megan Swenson <mrsmeganswenson@gmail.com>
2020-04-01 16:01:09 -0600

    Finished the styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,22 @@
 // Create a "topics" array of different kinds of topics
-var topics = ["Cats", "Dogs", "Horses", "Hamsters", "Birds"];
+var topics = ["The Office", "Parks and Rec", "Rick and Morty", "That 70's Show", "Breaking Bad", "Seinfeld", "Hey Arnold", "Sponge Bob"];
 
 // Function to create buttons for each item in the topics array and displays them in the #button-box div
 function renderButtons() {
     for (var i = 0; i < topics.length; i++) {
         button = $('<button>'+ topics[i] + '</button>');
         $(button).appendTo("#button-box");
-        button.addClass("animals");
+        button.addClass("tvshows");
+        button.attr("data-name", topics[i]);
     }
 }
 
 renderButtons();
 
 // On click function that retreives the top 10 gifs for the button that was clicked
-$(".animals").click(function() {
-    var animals = $(this).attr("data-animal");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "&api_key=brAL4zu8mGOUouJNI30dQQ68Zaji8rFY&limit=10";
+$(".tvshows").click(function() {
+    var tvShows = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvShows + "&api_key=brAL4zu8mGOUouJNI30dQQ68Zaji8rFY&limit=10";
 
     // AJAX call to get the gif data
     $.ajax({
@@ -23,34 +24,40 @@ $(".animals").click(function() {
         method: "GET"
     }).then(function(response) {
 
-        // Console.log response
-        console.log(response);
-
         // Create a var called results and set it equal to response.data
         var results = response.data;
+        console.log(results);
 
         // For loop for the results
         for (var x = 0; x < results.length; x++) {
-
-            // Create a div and store it in a var called topicsDiv
+            // Create a div and store it in a var called topicsDiv and put the image and rating into that div
             var topicsDiv = $("<div>");
-            // Create a paragraph tag and store it in a var called rating and set the text to the rating retreived
             var rating = $("<p>").text("Rating: " + results[x].rating);
-            console.log("Rating: " + results[x].rating);
-            // Create an image tag and store it in a var called topicsImage
             var topicsImage = $("<img>");
-            // Set the image's src to results[i]'s fixed_height.url.
             topicsImage.attr("src", results[x].images.fixed_height.url);
-            // Append the rating var to the topicsImage var
-            topicsImage.append(rating);
-            // Append the topicsImage var to the topicsDiv var
+            topicsDiv.append(rating);
             topicsDiv.append(topicsImage);
-            // Prepend the topicsDiv var to the #gifreturn-box div
+            topicsDiv.addClass("gif");
             $("#gifreturn-box").prepend(topicsDiv);
         }
-
     });
-
 });
 
 // On click function to start and stop the animation of the gifs
+$(".gif").click(function() {
+    // Create a var called state and store the image's data-state in it
+    var state = $(this).attr("data-state");
+    var still = $(this).attr("data-still");
+    var animate = $(this).attr("data-animate");
+    if (state === "still") {
+        state = "animate";
+        console.log("still");
+        $(this).attr("src", animate);
+        $(this).attr("data-state", state);
+    } else if (state === "animate") {
+        console.log("animate");
+        state = "still";
+        $(this).attr("src", still);
+        $(this).attr("data-state", state);
+    }
+});
